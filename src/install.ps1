c0b70dc24a964da1eb3e658f9e994f1f136de86b
C <Colton Meyers>
2023-01-22 12:51:01 -0800

    added install script and ls command

---
+++ src/install.ps1
@@ -0,0 +1,2 @@
+winget install -e --id Python.Python.3.10 # install python using winget
+pip install -r requirements.txt           # install all of the packages I used using pip
--- src/sql.py
+++ src/sql.py
@@ -4,11 +4,24 @@ import sys
 import re
 import pathlib
 from pynput import keyboard
+from pprint import pprint
 import os
 
-help_str = """ 
-	help will appear here
-"""
+help_str = """List of builtin commands:
+q | quit | exit
+	: quits program
+
+load "path"
+	: loads the structured data located at path into a sql table with a name corresponding to the filename
+	Example: load "../data/hw_25000.csv"
+
+ls
+	: lists all tables and their creation information
+
+Anything not matching the above commands will be treated as a SQLITE command and sent to the SQL engine for execution
+	Docs: https://www.sqlite.org/lang.html"""
+
+welcome_str = "Welcome to the SQL-REPL Environment.\nEnter 'help' for a list of all supported commands."
 
 # flushes the terminal input screen of all text
 # this allows you to write information where previous text information was displayed
@@ -29,11 +42,7 @@ class REPL():
 	first_line = "$:  "
 	multi_line = "... "
 
-	def debug(self, value : str):
-		if not self.debug:
-			print(value)
-
-	def __init__(self, database_path : str, debug = False) -> None:
+	def __init__(self, database_path : str) -> None:
 		self.con = sqlite3.connect(":memory:")
 
 		# prompt 
@@ -74,7 +83,7 @@ class REPL():
 
 	def eval_loop(self):
 		user_input_buf : list[str] = []
-
+		print(welcome_str)
 		while True:
 			user_input: list[str] = input(self.prompt_string).split()
 			match user_input:
@@ -82,8 +91,10 @@ class REPL():
 					print("Goodbye...")
 					self.con.close()
 					exit(0)
-				case ['help']:
+				case ['help'] | ['h']:
 					print(help_str)
+				case ['ls']:
+					self.print_all_tables()
 				# load "filepath"
 				case ['load', x] if re.match(r"\".*\"", x) is not None: 
 					file_path_without_quotes = x[1:len(x)-1]
@@ -105,7 +116,7 @@ class REPL():
 			if result is not None:
 				result_text_output = result.fetchall()
 				if result_text_output != []:
-					print(result_text_output)
+					[pprint(row) for row in result_text_output]
 		except Exception as e:
 			print("SQL EXCEPTION:")
 			print(e)
@@ -113,9 +124,24 @@ class REPL():
 
 		return True
 
+	def print_all_tables(self):
+		cursor = self.con.cursor()
+
+		result = cursor.execute("select name from sqlite_schema WHERE type IN ('table','view') AND name NOT LIKE 'sqlite_%'").fetchall()
+		if result is None or len(result) == 0:
+			print("No tables loaded")
+			return
+		for table in result:
+			# table[0] is needed because of some weird tuple packing
+			sql = f"SELECT sql FROM sqlite_master WHERE name='{table[0]}';"
+			print(sql)
+			table = cursor.execute(sql).fetchall()
+			pprint(table)
+
+
 	def load_external_table(self, filepath: str):
 		if not os.path.exists(filepath):
-			print("specified file does not exist")
+			print("fail specified file does not exist")
 			return
 
 		# no extension or path
@@ -127,7 +153,8 @@ class REPL():
 				# create the table
 				sql = f"create table {filename}("
 				for i, col in enumerate(df.columns):
-					sql += f"'{str(col).strip()}'"
+					col = str(col).strip().replace("\"","")
+					sql += f"'{col}'"
 					match str(df.dtypes[i]):
 						case "int64":
 							sql += " INTEGER"
@@ -137,29 +164,29 @@ class REPL():
 							sql += " TEXT"
 					sql += ","
 
+
 				sql = sql[:len(sql)-1]
 				sql += ")"
-				self.debug(sql)
+				print(sql)
 				self.con.execute(sql)
 
-				# insert the rows
-				for index, row in df.iterrows():
-					sql = f"insert into {filename} values ("
-					for i, cell in enumerate(row):
-						match str(df.dtypes[i]):
-							case "int64":
-								sql += f"{cell},"
-							case _:
-								sql += f"'{cell}',"
-					sql = sql[:len(sql)-1]
-					sql += ")"
-					self.debug(sql)
-					self.con.execute(sql)
+				# build a template string from which we can use to insert the rows
+				# ex : insert into foo values(?,?,?)
+				sql_insert_template = f"insert into {filename} values("
+				for _ in df.columns:
+					sql_insert_template += "?,"
+				sql_insert_template = sql_insert_template[:len(sql_insert_template)-1] + ")" # trim last comma
+
+				print(sql_insert_template)
+				# execute many re-uses the string and uses transaction control optimized for many sql statements
+				self.con.cursor().executemany(sql_insert_template, df.values.tolist())
+			case _:
+				print(f"unsupported filetype {pathlib.Path(filepath).suffix}")
+				return
+
 
 if __name__ == "__main__":
-
-	debug = len(sys.argv) > 0 and sys.argv[0] == 'debug'
-	repl = REPL(":memory:", debug=debug)
+	repl = REPL(":memory:")
 	repl.eval_loop()
 		
 
